4242ec72739ad6f048f90b7566ab600ce01a1ce0
vaibhav-rajale18 <vaibhavrajale2005@gmail.com>
2026-06-08 22:38:44 +0530

    Create middleware node component

--- src/components/canvas/FlowCanvas.jsx
+++ src/components/canvas/FlowCanvas.jsx
@@ -6,38 +6,51 @@ import ReactFlow, {
 import "reactflow/dist/style.css";
 
 import RequestNode from "../nodes/RequestNode";
+import MiddlewareNode from "../nodes/MiddlewareNode";
 
 const nodeTypes = {
   request: RequestNode,
+  middleware: MiddlewareNode,
 };
 
 const FlowCanvas = () => {
   const nodes = [
-    {
-      id: "1",
-      type: "request",
-      position: { x: 100, y: 150 },
-      data: {
-        label: "POST /login",
-      },
+  {
+    id: "1",
+    type: "request",
+    position: { x: 100, y: 150 },
+    data: {
+      label: "POST /login",
     },
-    {
-      id: "2",
-      type: "request",
-      position: { x: 450, y: 150 },
-      data: {
-        label: "GET /users",
-      },
+  },
+
+  {
+    id: "2",
+    type: "request",
+    position: { x: 450, y: 150 },
+    data: {
+      label: "GET /users",
     },
-    {
-      id: "3",
-      type: "request",
-      position: { x: 800, y: 150 },
-      data: {
-        label: "POST /register",
-      },
+  },
+
+  {
+    id: "3",
+    type: "request",
+    position: { x: 800, y: 150 },
+    data: {
+      label: "POST /register",
     },
-  ];
+  },
+
+  {
+    id: "4",
+    type: "middleware",
+    position: { x: 100, y: 350 },
+    data: {
+      label: "Auth Check",
+    },
+  },
+];
 
   return (
     <div className="h-full w-full">
--- src/components/nodes/MiddlewareNode.jsx
+++ src/components/nodes/MiddlewareNode.jsx
@@ -1,5 +1,14 @@
-function MiddlewareNode() {
-  return null;
+import BaseNode from "./BaseNode";
+
+function MiddlewareNode({ data }) {
+  return (
+    <BaseNode
+      title="Middleware"
+      label={data.label}
+      borderColor="border-yellow-500"
+      titleColor="text-yellow-400"
+    />
+  );
 }
 
 export default MiddlewareNode;
